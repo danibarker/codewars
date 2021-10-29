6d1090fadf5cb582fb7d86a71a349d448e65a2b9
danibarker <caseybarker86@gmail.com>
2021-10-28 18:06:25 -0600

    fixes

--- test/exercise1.test.js
+++ test/exercise1.test.js
@@ -1,7 +1,6 @@
 const chai = require("chai");
 const assert = chai.assert;
-chai.config.truncateThreshold=0;
-const countBy = require('../exercise1')
+const countBy = require('../exercises/exercise1')
 describe("Basic Tests",function(){
   it("Testing for fixed tests", () => {
     assert.deepEqual(countBy(1,10), [1,2,3,4,5,6,7,8,9,10], "Array does not match")
