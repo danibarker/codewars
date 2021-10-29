67467bc2e72e05405e35d42502e671f7d763e812
danibarker <caseybarker86@gmail.com>
2021-10-28 18:02:11 -0600

    init commit

---
+++ test/exercise1.test.js
@@ -0,0 +1,12 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+const countBy = require('../exercise1')
+describe("Basic Tests",function(){
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(countBy(1,10), [1,2,3,4,5,6,7,8,9,10], "Array does not match")
+    assert.deepEqual(countBy(2,5), [2,4,6,8,10], "Array does not match")
+    assert.deepEqual(countBy(9,5), [9,18,27,36,45], "Array does not match")
+    assert.deepEqual(countBy(100,3), [100,200,300], "Array does not match")
+  });
+});
